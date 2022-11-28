6a2228cb9e3aa41066562d2bdc748412dd81904c
ZHANARYS <nazarbakzhanarys@gmail.com>
2022-11-28 20:13:02 +0600

    add DZ1

---
+++ DZ1/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+
+/*
+Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+Console.Write("Введите 1-oe число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите 2-oe число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+    Console.Write($"max = {a}");
+else if (a == b)
+    Console.Write($"Числа равны {a}");
+else
+    Console.Write($"max = {b}");
+
+
